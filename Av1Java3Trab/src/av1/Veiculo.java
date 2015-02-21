211cf6cb2ffe68d14bd432356c8639fb758aa7e9
diegoNdutra <diego_rgk@hotmail.com>
2015-02-21 18:57:34 -0200

    Update Veiculo
    Criando as Variáveis

--- Av1Java3Trab/src/av1/Veiculo.java
+++ Av1Java3Trab/src/av1/Veiculo.java
@@ -1,5 +1,43 @@
 package av1;
 
 public class Veiculo {
-
+	private String chassi, montadora, modelo, tipo, cor;
+	private float preco;
+	
+	public String getChassi() {
+		return chassi;
+	}
+	public void setChassi(String chassi) {
+		this.chassi = chassi;
+	}
+	public String getMontadora() {
+		return montadora;
+	}
+	public void setMontadora(String montadora) {
+		this.montadora = montadora;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	public float getPreco() {
+		return preco;
+	}
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
 }
