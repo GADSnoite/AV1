ed4c0a4c787d8803a8c1345cf5e25ec5bd7f41bc
diegoNdutra <diego_rgk@hotmail.com>
2015-02-21 19:05:07 -0200

    Classe motocicleta criada com suas Variaveis

---
+++ Av1Java3Trab/src/av1/Motocicleta.java
@@ -0,0 +1,22 @@
+package av1;
+
+public class Motocicleta extends Veiculo{
+	private int cilindrada, capacidadeTanque;
+
+	public int getCilindrada() {
+		return cilindrada;
+	}
+
+	public void setCilindrada(int cilindrada) {
+		this.cilindrada = cilindrada;
+	}
+
+	public int getCapacidadeTanque() {
+		return capacidadeTanque;
+	}
+
+	public void setCapacidadeTanque(int capacidadeTanque) {
+		this.capacidadeTanque = capacidadeTanque;
+	}
+	
+}
