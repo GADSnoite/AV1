95ae6c7bf7b894f5267252338a5cd2fea6e2cb91
diegoNdutra <diego_rgk@hotmail.com>
2015-02-21 18:53:14 -0200

    Update Carro
    Criando Variáveis

--- Av1Java3Trab/src/av1/Carro.java
+++ Av1Java3Trab/src/av1/Carro.java
@@ -1,5 +1,20 @@
 package av1;
 
-public class Carro {
-
+public class Carro extends Veiculo{
+	private String cambio;
+	private float motorizacao;
+	
+	public String getCambio() {
+		return cambio;
+	}
+	public void setCambio(String cambio) {
+		this.cambio = cambio;
+	}
+	public float getMotorizacao() {
+		return motorizacao;
+	}
+	public void setMotorizacao(float motorizacao) {
+		this.motorizacao = motorizacao;
+	}
+	
 }
